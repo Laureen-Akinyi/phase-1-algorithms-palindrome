36199c853365e4e804a5b373b9c26d0ca14a1052
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-19 23:23:14 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+ let palindromeWord = word.split("").reverse().join("")
+ if (word == palindromeWord){
+   return true
+ } else{
+   return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  The function takes one argument
+  If the argument passed return true 
+  
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  I declared a variable, palindromeWord which splits reverses and joins the word passed back to one word
+  If the word matches it returns true
+  else retuens false
+
 */
 
 // You can run `node index.js` to view these console logs
